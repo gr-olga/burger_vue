7422ac53cca2c90ff8df6430ffdb1d268ac88f2e
Olga Panfilova <gr-olga@ya.ru>
2024-05-24 09:25:58 +0200

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="hello">
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .hello {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
